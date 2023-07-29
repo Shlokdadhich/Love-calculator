cdb41ac8c85c26059c5f2043f8e38a43148cfaa7
Shlokdadhich <117933293+Shlokdadhich@users.noreply.github.com>
2023-07-29 16:56:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("ram ram")
+var x=Math.floor(Math.random()*100+1)
+document.querySelector(".percent").innerHTML=x+"%"
+let n1=document.querySelector("#name2");
+console.log(n1)
---
+++ next.js
@@ -0,0 +1,5 @@
+var n1=document.getElementById('name1');
+console.log(n1)
+var x=Math.floor(Math.random()*100+1)
+document.querySelector(".percent").innerHTML=x+"%"
+document.querySelectorAll("p")[0].innerHTML=n1
